a5c6de9cdbbbfe2c8a1917ee22e69f82a4ef83e3
JazzMedo <mohamedmelawany@hotmail.com>
2024-05-07 10:29:28 +0300

    last

--- lib/ProjectsPage.dart
+++ lib/ProjectsPage.dart
@@ -181,6 +181,89 @@ class _ProjectsPageState extends State<ProjectsPage> {
                     ],
                   ),
                 )),
+          ),
+          const SizedBox(
+            height: 30,
+          ),
+          SizedBox(
+            height: 160,
+            child: Card(
+                elevation: 7,
+                child: Padding(
+                  padding: const EdgeInsets.all(15),
+                  child: Row(
+                    children: [
+                      Column(
+                        children: [
+                          Image.asset(
+                            "img/h.png",
+                            width: 35,
+                            height: 35,
+                          ),
+                          Image.asset(
+                            "img/c.png",
+                            width: 50,
+                            height: 50,
+                          ),
+                          Image.asset(
+                            "img/jj.png",
+                            width: 30,
+                            height: 30,
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        width: 25,
+                      ),
+                      const Column(
+                          crossAxisAlignment: CrossAxisAlignment.start,
+                          children: [
+                            Text(
+                              "Tickets Project",
+                              style: TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text(
+                              "Making a Reliable Tickets\nSystem Website that \nmanages tickets for events",
+                              textAlign: TextAlign.left,
+                              style: TextStyle(
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.normal,
+                                  color: Color.fromARGB(255, 78, 78, 78)),
+                            ),
+                          ]),
+                      const SizedBox(
+                        width: 32,
+                      ),
+                      const Column(
+                          crossAxisAlignment: CrossAxisAlignment.center,
+                          children: [
+                            SizedBox(
+                              height: 25,
+                            ),
+                            Text(
+                              "Price:",
+                              style: TextStyle(
+                                  fontSize: 20, fontWeight: FontWeight.bold),
+                            ),
+                            SizedBox(
+                              height: 10,
+                            ),
+                            Text(
+                              "159\$",
+                              textAlign: TextAlign.left,
+                              style: TextStyle(
+                                  fontSize: 20,
+                                  fontWeight: FontWeight.bold,
+                                  color: Color.fromARGB(255, 14, 163, 0)),
+                            ),
+                          ])
+                    ],
+                  ),
+                )),
           )
         ],
       )),
